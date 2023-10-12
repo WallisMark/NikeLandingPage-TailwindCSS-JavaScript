71ce70088c5b2adf0002b7f60eaba5ebfdcb8f1b
wallis <wallismark@gmail.com>
2023-10-12 15:37:00 +0200

    hello  world

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    
+      <h2 className='text-3xl font-bold underline'>Hello World this is me starting </h2>
+    
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+  // tailwind.config.js
+  // module.exports = {
+
+  //   purge: [],
+ 
+  //   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  //    darkMode: false, // or 'media' or 'class'
+  //    theme: {
+  //      extend: {},
+  //    },
+  //    variants: {
+  //      extend: {},
+  //    },
+  //    plugins: [],
+  //  }
